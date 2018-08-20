7abc8774bbb91c523b90fd24c56265dcb839898f
Pandaradox <jacardarella@gmail.com>
2018-08-20 11:51:30 -0500

    Modified beaker

--- beaker.py
+++ beaker.py
@@ -1,8 +1,33 @@
-def fart(x):
-    """This is a function for generating farts"""
-    for toots in range(x):
-        print("Toot")
+##################################################
+#
+# def fart(x):
+#     """This is a function for generating farts"""
+#     for toots in range(x):
+#         print("Toot")
+#     if (x >= 10):
+#         print("IMPRESSIVE!")
+#
+# x = int(input("How many beans did you eat? "))
+# fart(x)
+#
+# print(fart.__doc__)
+#
 
-x = int(input("How many beans did you eat? "))
-fart(x)
-print(fart.__doc__)
+##################################################
+#
+# import turtle, random
+# tess=turtle.Turtle()
+# wn=turtle.Screen()
+# rainbow = ["red", "orange", "yellow", "green", "blue", "violet"]
+# tess.penup()
+# tess.goto(-300,0)
+# for sides in (3,4,6,8):
+#    tess.penup()
+#    tess.forward(100)
+#    tess.pendown()
+#    for i in range(sides):
+#        tess.pencolor(random.choice(rainbow))
+#        tess.forward(40)
+#        tess.left(360/sides)
+# wn.exitonclick()
+##################################################
