0636a1a4bb933c77d3eb22fd67c762b64ec4c7e3
Pandaradox <jacardarella@gmail.com>
2018-09-04 13:41:39 -0500

    Init of week4

--- beaker.py
+++ beaker.py
@@ -2,10 +2,32 @@
 ##################################################
 
 
-def main():
+def course_grader(test_scores):
+   add_tests = 0
+   for test in test_scores:
+       if test <= 50:
+           print("fail1")
+       else:
+           add_tests += test
+           average = add_tests / len(test_scores)
+   print(average)
+   if average >= 70:
+       print("pass")
+   else:
+       print("fail2")
 
-    print("Ham and Eggs")
-    
+def main():
+   test_scores = [80,60,60]
+   course_grader(test_scores)
+   print(print("Hello"))
+
+
+
+   print(course_grader([100,75,45]))     # "fail"
+   print(course_grader([100,70,85]))     # "pass"
+   print(course_grader([80,60,60]))      # "fail"
+   print(course_grader([80,80,90,30,80]))  # "fail"
+   print(course_grader([70,70,70,70,70]))  # "pass"
 
 if __name__ == "__main__":
-    main()
+   main()
