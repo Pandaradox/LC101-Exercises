5162dd7b98d4205aaf4c6a5085dbe6b4d388b0dd
Pandaradox <jacardarella@gmail.com>
2018-09-14 12:43:03 -0500

    week5 final

--- ch9exercises.py
+++ ch9exercises.py
@@ -72,36 +72,3 @@
 #
 # if __name__ == "__main__":
 #     main()
-
-# ##################################################Studio: Bubble Sort
-
-
-def bubble_sort(bubble):
-    if len(bubble) == 1:
-        return(bubble)
-    x = 0
-    while x == 0:
-        for i in range(len(bubble)-1):
-            if bubble[i] > bubble[i+1]:
-                y = bubble[i]
-                bubble[i] = bubble[i+1]
-                bubble[i+1] = y
-        for i in range(len(bubble)-1):
-            if bubble[i] > bubble[i+1]:
-                x == 0
-                break
-            else:
-                x = 1
-    return(bubble)
-
-
-def main():
-
-    print(bubble_sort([0]), [0])
-    print(bubble_sort([1, 2, 3, 4, 5, 3, 5, 6, 7]), [1, 2, 3, 4, 5])
-    print(bubble_sort([5, 4, 3, 2, 1]), [1, 2, 3, 4, 5])
-    print(bubble_sort([4, 5, 3, 1, 2]), [1, 2, 3, 4, 5])
-
-
-if __name__ == "__main__":
-    main()
